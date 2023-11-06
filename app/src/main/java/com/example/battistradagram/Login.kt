86c973d98a96373494179f1ed7308bd4ea57bbb3
Anugerah Gumelar <agum@Anugerahs-MacBook-Pro.local>
2023-11-06 16:15:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/battistradagram/Login.kt
@@ -0,0 +1,42 @@
+package com.example.battistradagram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.battistradagram.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            login()
+        }
+    }
+
+    private fun login() {
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+
+        if (validateLogin(email,password)){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateLogin(email: String, password: String): Boolean {
+        return email == "admin" && password == "password"
+
+    }
+
+}
